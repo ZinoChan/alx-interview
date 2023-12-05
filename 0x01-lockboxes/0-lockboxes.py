585a63917dae79466c5e976b46919fd2d289cc72
ZinoChan <devzinochan@gmail.com>
2023-12-05 19:20:26 +0100

    handle keys with no boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,11 +6,10 @@ open All boxes
 
 def canUnlockAll(boxes):
     """check if all boxes can be opened"""
-    if len(boxes) == 0:
-        return False
     box_set = set(boxes[0])
     for n in boxes[0]:
-        box_set.update(boxes[n])
+        if n < len(boxes):
+            box_set.update(boxes[n])
 
     for i in range(1, len(boxes)):
         if i in box_set:
