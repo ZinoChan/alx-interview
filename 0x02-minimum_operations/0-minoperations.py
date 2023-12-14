7864b24c49806937dcae76c5ed1fdfbecc4e73a7
ZinoChan <devzinochan@gmail.com>
2023-12-14 12:44:45 +0100

    fix typo

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,11 +2,11 @@
 """Min operation to print n of H"""
 
 
-def min_op(n, curr=2, topast=1, op=2):
+def minOperations(n, curr=2, topast=1, op=2):
     """Min Operation"""
     if n <= 1:
         return 0
     if curr >= n:
         return op
-    return min(min_op(n, curr + curr, curr, op + 2),
-               min_op(n, curr + topast, topast, op + 1))
+    return min(minOperations(n, curr + curr, curr, op + 2),
+               minOperations(n, curr + topast, topast, op + 1))
