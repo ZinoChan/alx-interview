ef1450d0dee664edd6bf53ed12e40fb5822ea9b6
ZinoChan <devzinochan@gmail.com>
2023-12-14 13:02:25 +0100

    use prime factorizatio

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,12 +4,13 @@
 
 def minOperations(n):
     """Min Operation"""
-    if n <= 1:
-        return 0
-    dp = [0] * (n + 1)
-    dp[1] = 1
-    for i in range(2, n+1):
-        min_copy_past = dp[i // 2] + 2
-        min_past_prev = dp[i - 1] + 1
-        dp[i] = min(min_copy_past, min_past_prev)
-    return dp[n]
+    total_operations = 0
+    current_factor = 2
+
+    while n > 1:
+        while n % current_factor == 0:
+            total_operations += current_factor
+            n /= current_factor
+        current_factor += 1
+
+    return total_operations
