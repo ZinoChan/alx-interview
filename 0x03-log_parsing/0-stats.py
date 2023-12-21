185bad4fe0e239fb490377b9a99a2b5ee366ef1d
ZinoChan <devzinochan@gmail.com>
2023-12-21 14:38:21 +0100

    refactor

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -47,6 +47,5 @@ try:
 
         if line_count % 10 == 0:
             print_metrics(total_file_size, status_code_counts)
-
-except KeyboardInterrupt:
+finally:
     print_metrics(total_file_size, status_code_counts)
