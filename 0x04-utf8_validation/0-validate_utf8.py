93bc91376abfb1f5a2ac676ca51bd174022b0dff
ZinoChan <devzinochan@gmail.com>
2024-01-04 22:28:47 +0100

    add validator

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,22 @@
+def validUTF8(data):
+    """validate utf8"""
+    def is_continuation(byte):
+        """helper"""
+        return (byte & 0b11000000) == 0b10000000
+
+    i = 0
+    while i < len(data):
+        leading_ones = 0
+        while (data[i] >> (7 - leading_ones)) & 1:
+            leading_ones += 1
+
+        if leading_ones == 1 or leading_ones > 4:
+            return False
+
+        for j in range(1, leading_ones):
+            if i + j >= len(data) or not is_continuation(data[i + j]):
+                return False
+
+        i += leading_ones
+
+    return True
