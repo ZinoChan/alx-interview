16314b398d3f5eea1ab7a0913b4560a6fb6653b3
ZinoChan <devzinochan@gmail.com>
2024-01-11 20:24:00 +0100

    refactor

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -17,14 +17,17 @@ def is_safe(placed_queens, row, col):
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
+        print("Usage: nqueens N")
         sys.exit(1)
 
     try:
         n = int(sys.argv[1])
     except ValueError:
+        print('N must be a number')
         exit(1)
 
     if n < 4:
+        print('N must be at least 4')
         exit(1)
 
     solutions = []
