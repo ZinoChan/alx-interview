ef18b10ad8f8e531db1537730722f7c4f39ff56f
ZinoChan <devzinochan@gmail.com>
2024-02-05 21:37:39 +0100

    init

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+def islandPerimeter(grid: List[List[int]]) -> int:
+    row, column = len(grid), len(grid[0])
+    perimeter = 0
+    for i in range(row):
+        for j in range(column):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i-1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter -= 2
+    return perimeter
