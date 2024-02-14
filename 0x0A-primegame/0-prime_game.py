65087a91131251dfceb669643ee2f16892051b4f
ZinoChan <devzinochan@gmail.com>
2024-02-14 15:21:30 +0100

    refactor

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python3
 """
-Defines isWineer
+define is winner
 """
 
 
-def isWinner(x, nums):
-    """Determines the winner of multiple rounds of the prime number game."""
+def getPrimes(n):
+    """ list of primes"""
+    prime = []
+    sieve = [True] * (n + 1)
+    for p in range(2, n + 1):
+        if (sieve[p]):
+            prime.append(p)
+            for i in range(p, n + 1, p):
+                sieve[i] = False
+    return prime
 
+
+def isWinner(x, nums):
+    """
+        define winner
+    """
     if x is None or nums is None or x == 0 or nums == []:
         return None
     maria_count = ben_count = 0
-    for n in nums:
-        prime_list = []
-        sieve = [True] * (n + 1)
-        for p in range(2, n + 1):
-            if sieve[p]:
-                prime_list.append(p)
-                for i in range(p, n + 1, p):
-                    sieve[i] = False
-
-        if len(prime_list) % 2 == 0:
+    for i in range(x):
+        prime = getPrimes(nums[i])
+        if len(prime) % 2 == 0:
             ben_count += 1
         else:
             maria_count += 1
